1bbc82a33561cff03b3175bf342774540b74d357
jobananada12 <160485496+jobananada12@users.noreply.github.com>
2026-08-25 17:17:20 +0300

    Use mega curriculum app entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from './MegaApp.jsx';
 import './native-tts.js';
 
 createRoot(document.getElementById('root')).render(<App />);
